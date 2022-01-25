13363009a995b3cd8cf25057c40079eb89b0abca
Tom-Deganck <tom.deganck@vlot.be>
2022-01-25 15:38:43 +0100

    Add project files.

---
+++ HMI_Robbe_Tom/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HMI_Robbe_Tom
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
